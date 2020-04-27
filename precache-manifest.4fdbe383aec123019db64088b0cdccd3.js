865b6f420bc49bd46dc61fc3795ab06875459267
Catalin <lungancatalin@yahoo.com>
2020-04-27 20:37:19 +0300

    Updates

---
+++ precache-manifest.4fdbe383aec123019db64088b0cdccd3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7ed529011e5e81ac44fdb37184ce1c",
+    "url": "/fylo/index.html"
+  },
+  {
+    "revision": "178444394a6470a5523a",
+    "url": "/fylo/static/css/main.fef066b4.chunk.css"
+  },
+  {
+    "revision": "e2c4120e72aecf29a7d7",
+    "url": "/fylo/static/js/2.bdec776a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fylo/static/js/2.bdec776a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178444394a6470a5523a",
+    "url": "/fylo/static/js/main.d457e8ae.chunk.js"
+  },
+  {
+    "revision": "2bcf637b846fd69e6fd7",
+    "url": "/fylo/static/js/runtime-main.7df791e1.js"
+  },
+  {
+    "revision": "e9fdc10442430eaa6f39fde1ece21ac0",
+    "url": "/fylo/static/media/icon-arrow.e9fdc104.svg"
+  },
+  {
+    "revision": "a51e688006d6bdc00db7e8fb4f923081",
+    "url": "/fylo/static/media/icon-quotes.a51e6880.svg"
+  },
+  {
+    "revision": "b54a697161d657d5e1a154b7e589fb65",
+    "url": "/fylo/static/media/illustration-1.b54a6971.svg"
+  },
+  {
+    "revision": "13d8305b15e4b62000c2e2c756b5e70c",
+    "url": "/fylo/static/media/illustration-2.13d8305b.svg"
+  },
+  {
+    "revision": "0786a6202db9d6278c6b043c53bcc3ad",
+    "url": "/fylo/static/media/logo.0786a620.svg"
+  },
+  {
+    "revision": "6671a399292f1c4228756c267006fbf0",
+    "url": "/fylo/static/media/logo_white.6671a399.svg"
+  }
+]);
